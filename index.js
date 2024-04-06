d357e7dc44bf92f8b62aa5e98573b98c302d9b2f
Elsaigh <elsaigh1@outlook.com>
2024-04-06 04:41:42 -0400

    Add geocoding and what to wear algorithm

--- index.js
+++ index.js
@@ -12,24 +12,62 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", async (req, res) => {
+  res.render('index.ejs')
+})
+
+app.post('/location', async (req, res) => {
+
+  const city = req.body.city
+  const country = req.body.country
+
+  var geocodeJSON
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${43.65}&lon=${-79.38}&appid=${key}`)
-    const result = response.data
-    console.log(result)
-    const temp = result.main.temp
-    const atmosphere = result.weather[0].main
-    let wear = ""
-    if (atmosphere === "Rain" || atmosphere === "Drizzle" || atmosphere === "Thunderstorm") {
-      wear = "Waterproof Jacket"
-    } else if (atmosphere === "Snow") {
-      wear = "Winter Jacket"
-    } else {
-      wear = "Normal Wear"
-    }
-    res.render("index.ejs", { temp: temp, wear: wear })
+    const geocodeResponse = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`)
+    geocodeJSON = geocodeResponse.data
   } catch (error) {
-    res.render("index.ejs", { error: error.message })
+    geocodeJSON = []
   }
+
+  var lat, lon
+  if (geocodeJSON.length === 0) {
+    lat = 43.65
+    lon = -79.38
+  } else {
+    lat = geocodeJSON[0].lat
+    lon = geocodeJSON[0].lon
+  }
+  console.log(geocodeJSON)
+
+  const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${key}`)
+  const weatherJSON = weatherResponse.data
+  //console.log(weatherJSON)
+
+  const temp = weatherJSON.main.temp
+  const atmosphere = weatherJSON.weather[0].main
+  const location = weatherJSON.name
+
+  var clothesResult
+  switch (atmosphere) {
+    case 'Rain':
+    case 'Drizzle':
+    case 'Thunderstorm':
+      clothesResult = "Waterproof Jacket"
+      break;
+    case 'Snow':
+      clothesResult = "Heavy Winter Jacket"
+      break;
+    default:
+      clothesResult = "Normal Light Clothes"
+      if (temp < 10) {
+        clothesResult = "Warm Clothes"
+      }
+  }
+
+  res.render("index.ejs", { 
+    temp: temp,
+    wear: clothesResult,
+    location: location 
+  })
 })
 
 app.listen(port, () => {
